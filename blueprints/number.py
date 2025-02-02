a76e43b64d3700a3253c43428fb9502f5837d190
棋 <imqi1@qq.com>
2025-02-02 13:18:41 +0800

    完善数字功能

--- blueprints/number.py
+++ blueprints/number.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from core.db import Session, Number
 
 number_blueprint = Blueprint('number', __name__)
@@ -6,10 +6,28 @@ number_blueprint = Blueprint('number', __name__)
 
 @number_blueprint.route('/get', methods=['GET'])
 def get_number():
-    with Session() as session:
+    with (Session() as session):
         try:
             number = session.query(Number.number).first()
             if number:
+                return {'number': number.number}
+            else:
+                session.add(Number(number=0))
+                session.commit()
+                return {'number': 0}
+        except Exception as e:
+            session.rollback()
+            return {'error': str(e)}, 500
+
+
+@number_blueprint.route('/set', methods=['GET'])
+def set_number():
+    with Session() as session:
+        number = request.args.get('number', 0, type=int)
+        try:
+            number = session.query(Number).first()
+            if number:
+                number.number = 0
                 return {'number': number.number}
             else:
                 session.add(Number(number=0))
