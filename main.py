523557db8844b7b8ccca76692b82206fca912a10
棋 <imqi1@qq.com>
2025-02-03 01:20:48 +0800

    修改项目依赖为 Flask-SQLAlchemy 和 Flask-Migrate

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ from app.extensions import db
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+    print("尝试创建数据库")
     app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
