73c884a54fa1f4e8fca5eb1ac556641470894f2e
棋 <imqi1@qq.com>
2025-02-05 00:00:42 +0800

    增加用户名注册

--- app/__init__.py
+++ app/__init__.py
@@ -6,32 +6,43 @@ from app.extensions import db, migrate
 
 app = Flask(__name__)
 
-app.config.from_object('app.config.Config')
+app.config.from_object("app.config.Config")
 
 db.init_app(app)
 migrate.init_app(app, db)
-app.register_blueprint(number_blueprint, url_prefix='/number')
-app.register_blueprint(user_blueprint, url_prefix='/user')
+app.register_blueprint(number_blueprint, url_prefix="/number")
+app.register_blueprint(user_blueprint, url_prefix="/user")
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return render_template('index.html', title="毕设后端")
+    return render_template("index.html", title="毕设后端")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "", 404
+
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return "", 405
 
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     return response
+
 
 @app.before_request
 def before_request():
-    referer = request.headers.get('Referer')
+    referer = request.headers.get("Referer")
     if not referer:
         # return response
         return
     if not referer.startswith(config.frontend):
-        return render_template("403.html", title="403"), 403
+        return "", 403
     return
