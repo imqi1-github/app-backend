1a17fead0afecbec8ee1696343a957dd0880bb9e
棋 <imqi1@qq.com>
2025-02-05 00:02:25 +0800

    增加用户名注册

--- app/__init__.py
+++ app/__init__.py
@@ -31,7 +31,7 @@ def method_not_allowed(e):
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Origin"] = config.frontend
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     return response
