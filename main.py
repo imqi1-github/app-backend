20fec3d04841e6a65ca96ac748652fe4cafeb20c
棋 <imqi1@qq.com>
2025-02-04 14:58:30 +0800

    增加用户名注册

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
+import subprocess
 
 from app import app
-from app.extensions import db
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    print("尝试创建数据库")
+    subprocess.call(["sh", "./migrate.sh"])
     app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
