4d6856c28c331d93f0643221b35b34ff4747d7f6
棋 <imqi1@qq.com>
2025-02-04 23:53:08 +0800

    增加用户名注册

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from app.blueprints import number_blueprint, user_blueprint
 from app.config import config
@@ -25,3 +25,13 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     return response
+
+@app.before_request
+def before_request():
+    referer = request.headers.get('Referer')
+    if not referer:
+        # return response
+        return
+    if not referer.startswith(config.frontend):
+        return render_template("403.html", title="403"), 403
+    return
